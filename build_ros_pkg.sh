38f5c728e47f37e9156c37897349af915bcb0b59
Oscar Viquez <oviquezr@acbotics.com>
2024-04-03 12:15:26 -0400

    fix build script wth updated submodule setup

--- build_ros_pkg.sh
+++ build_ros_pkg.sh
@@ -4,6 +4,21 @@
 git submodule init
 git submodule update
 
+ROOTDIR=$(dirname $0)
+echo "Root dir is : $ROOTDIR"
+if [ -d $ROOTDIR/src/acsense_pubsub/acbotics_interface ]; then
+    rm -rf $ROOTDIR/src/acsense_pubsub/acbotics_interface
+fi
+if [ -d $ROOTDIR/src/acsense_pubsub/icontract ]; then
+    rm -rf $ROOTDIR/src/acsense_pubsub/icontract
+fi
+if [ -d $ROOTDIR/src/acsense_pubsub/asttokens ]; then
+    rm -rf $ROOTDIR/src/acsense_pubsub/asttokens
+fi
+cp -r $ROOTDIR/submodules/icontract/icontract $ROOTDIR/src/acsense_pubsub/
+cp -r $ROOTDIR/submodules/asttokens/asttokens $ROOTDIR/src/acsense_pubsub/
+cp -r $ROOTDIR/submodules/acbotics_interface/src/acbotics_interface $ROOTDIR/src/acsense_pubsub/
+
 # Get rosdep dependencies
 rosdep install -i --from-path src --rosdistro noetic -y
 
